a256b7dbe07ff19f600ee7bd371b453272ad8b1f
William-sheesh <2102278@ub.edu.ph>
2026-07-20 21:53:58 +0800

    Progress: Connected frontend to wallet

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@x402/core/client': false,
+      '@x402/evm/exact/client': false,
+      '@x402/evm/upto/client': false,
+      '@x402/svm/exact/client': false,
+      '@x402/evm': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
